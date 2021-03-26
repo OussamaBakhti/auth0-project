7aac50a7f5e82dd087eaa3b51999f1a7844d70cf
Administrator <admin@example.com>
2021-03-26 19:48:27 +0100

    auth works fine need database to add

--- index.js
+++ index.js
@@ -62,16 +62,16 @@ app.use('/api/users', usersRouter);
 
 
 app.use((req, res, next) => {
-    const error = new Error('resource not found');
-    error.status = 404;
-    next(error);
-  });
+  const error = new Error('resource not found');
+  error.status = 404;
+  next(error);
+});
   
-  app.use((error, req, res, next) => {
-    // console.log(error);
-    res.status(error.status || 422).send({
-      message: error.message
-    });
+app.use((error, req, res, next) => {
+  // console.log(error);
+  res.status(error.status || 422).send({
+    message: error.message
   });
+});
   
   module.exports = app;
