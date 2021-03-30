74158a34b818b0d34868582ac3b61b9265b2043a
Administrator <admin@example.com>
2021-03-30 14:09:23 +0100

    sgh

--- api/models/config.js
+++ api/models/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createPool({
     host: 'localhost',
-    user: 'root',
+    user: '',
     password: '',
     database: 'testauth',
     waitForConnections: true,
